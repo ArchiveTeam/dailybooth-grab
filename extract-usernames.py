40f6b013b44b204e09fb017ac5edca430de85767
Alard <alard@example.com>
2012-12-16 23:42:06 +0100

    Python 2.6 (on the warrior) doesn't do 'with gzip'.

--- extract-usernames.py
+++ extract-usernames.py
@@ -10,13 +10,14 @@ seen = set()
 print >> sys.stderr, "Extracting usernames from %s" % sys.argv[1]
 
 with open(sys.argv[2], "w") as f_out:
-  with gzip.open(sys.argv[1]) as f_in:
-    for line in f_in:
-      for a, b in USERNAME_LINK_RE.findall(line):
-        if a == b and not a in seen:
-          seen.add(a)
-          print >> f_out, a
-          sys.stderr.write("\rFound: %d" % len(seen))
+  f_in = gzip.open(sys.argv[1])
+  for line in f_in:
+    for a, b in USERNAME_LINK_RE.findall(line):
+      if a == b and not a in seen:
+        seen.add(a)
+        print >> f_out, a
+        sys.stderr.write("\rFound: %d" % len(seen))
+  f_in.close()
 
 n = len(seen)
 print >> sys.stderr, "\rExtracted %d username%s." % (n, ("" if n==1 else "s"))
