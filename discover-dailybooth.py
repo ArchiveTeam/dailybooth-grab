941ec72fe776f5b415ab0786ace1d7dcbbc71236
Alard <alard@example.com>
2012-12-21 17:49:03 +0100

    New approach to find usernames.

---
+++ discover-dailybooth.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import json
+import re
+import time
+import urllib
+import sys
+
+from tornado import ioloop, httpclient, gen
+
+
+
+USER_AGENT = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27"
+http_client = httpclient.HTTPClient()
+ahttp_client = httpclient.AsyncHTTPClient()
+
+print "Loading task..."
+res = http_client.fetch("http://tracker.archiveteam.org:8126/request", method="POST", body="version=1")
+task = json.loads(res.body)
+# task = { "prefix": "10000", "usernames": ["xyz","api","charles"] }
+
+if task["prefix"] == None:
+  print "No task."
+  exit()
+
+usernames = task["usernames"]
+prefix = task["prefix"]
+print "Task %s" % prefix
+
+
+usernames_found = set()
+queue = [ i for i in range(0,100) ]
+running = [ 0 ]
+
+def handle_request(response):
+  if response.code == 302:
+    # redirect to real username!
+    usernames_found.add(response.headers['Location'].split("/")[-1])
+  elif response.code >= 500 and response.code <= 599:
+    print "\nError %d" % response.code
+    queue.append(response.request.i)
+
+  running[0] = running[0] - 1
+  next_from_queue()
+
+def next_from_queue():
+  if len(queue) > 0:
+    i = queue.pop()
+    sys.stdout.write("\r%d  " % i)
+    sys.stdout.flush()
+
+    req = httpclient.HTTPRequest(
+        ("http://dailybooth.com/%s/%s%02d" % (usernames[i % len(usernames)], prefix, i)),
+        connect_timeout=10, request_timeout=30,
+        follow_redirects=False,
+        user_agent=USER_AGENT)
+    ahttp_client.fetch(req, handle_request)
+    running[0] = running[0] + 1
+
+  elif running[0] == 0:
+    ioloop.IOLoop.instance().stop()
+
+next_from_queue()
+next_from_queue()
+next_from_queue()
+
+ioloop.IOLoop.instance().start()
+
+
+sys.stdout.write("\n")
+
+usernames_found = [ u for u in usernames_found ]
+# print usernames_found
+
+print
+print "Submitting results (%d usernames)..." % (len(usernames_found))
+json_body = json.dumps({ "prefix": task["prefix"], "usernames_found": usernames_found })
+# print json_body
+res = http_client.fetch("http://tracker.archiveteam.org:8126/submit", method="POST",
+                        body=json_body, headers={"Content-Type": "application/json"})
+
+
